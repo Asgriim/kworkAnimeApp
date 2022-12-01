ecd83b3d9c25020c4ffbdc7967dffdb81a0eed36
Xlebuchek <vagaev.ar@yandex.ru>
2022-12-01 18:29:57 +0300

    git pidor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+        System.out.println("git pidor blyat");
 
         launch();
 //        System.out.println("bebra");
